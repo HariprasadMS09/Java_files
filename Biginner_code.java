e524d99d909110a15a07339de7280c64c5bdee9b
HariprasadMS09 <hariprasadms01@gmail.com>
2024-09-18 15:42:27 +0530

    Added new line of code

--- Biginner_code.java
+++ Biginner_code.java
@@ -4,5 +4,7 @@ public class Biginner_code
    public static void main(String args[])
    {
         System.out.println("Helo World!");
+        System.out.println("welcome");
+        System.out.println("shata");
    } 
 }
