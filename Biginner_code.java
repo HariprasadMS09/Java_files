61d847f1e19fb236e3652ecde031f1caa2216f82
HariprasadMS09 <hariprasadms01@gmail.com>
2024-09-18 20:07:13 +0530

    Updated first line of code

--- Biginner_code.java
+++ Biginner_code.java
@@ -3,7 +3,7 @@ public class Biginner_code
 {
    public static void main(String args[])
    {
-        System.out.println("Helo World!");
+        System.out.println("Helo World. GM!");
         System.out.println("welcome");
         System.out.println("shata");
    } 
